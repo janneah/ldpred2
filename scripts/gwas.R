03e1d4da2b6caeea72ec61a9df83ea342a108ec3
Janne Auning Hansen <79196745+janneah@users.noreply.github.com>
2024-02-13 09:57:24 +0100

    Added comments

--- scripts/gwas.R
+++ scripts/gwas.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(data.table)
 
 # Reading in data
-dosage <- snp_attach(dosage_path)                # Attach a "bigSNP" from backing files
+dosage <- snp_attach(dosage_path)                # Load a "bigSNP" object from backing files
 ipsych_design <- fread(ipsych_design_path)       # Meta data file containing info on the iPSYCH design from the registers
 phenotype_info <- fread(phenotype_path)
 
